1cbd412a21e947c6ca2ab0070911dc3ef4e666d6
Mofundo1 <promowave247@gmail.com>
2024-10-20 03:14:22 -0400

    Update random-picture.js

--- pages/api/random-picture.js
+++ pages/api/random-picture.js
@@ -1,9 +1,9 @@
 export default function handler(req, res) {
   // Array of picture URLs (replace these with your actual image links)
   const pictures = [
-    'https://example.com/image1.jpg',
-    'https://example.com/image2.jpg',
-    'https://example.com/image3.jpg'
+    '"https://drive.google.com/file/d/1ZmxZ0-rU0edjWiPJWFz-U9-AC_Uss836/view?usp=drive_link',
+    'https://drive.google.com/file/d/1ZmxZ0-rU0edjWiPJWFz-U9-AC_Uss836/view?usp=drive_link',
+    'https://drive.google.com/file/d/1uMIGxyzYeEldqIoaM1xatj4Kklex8qXD/view?usp=drive_link'
   ];
 
   // Select a random picture from the array
